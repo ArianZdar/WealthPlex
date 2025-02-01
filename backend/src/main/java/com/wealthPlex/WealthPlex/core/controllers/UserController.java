749e425d97828d4fc1b6345136797f2ba64a9bf4
hamzadaqa <abudaqahamza@gmail.com>
2025-02-01 15:37:12 -0500

    Finished code for the OPENAi raking system

--- backend/src/main/java/com/wealthPlex/WealthPlex/core/controllers/UserController.java
+++ backend/src/main/java/com/wealthPlex/WealthPlex/core/controllers/UserController.java
@@ -1,6 +1,5 @@
 package com.wealthPlex.WealthPlex.core.controllers;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
