b9359ae603dda7d4ba145fd16f7ffd50c723df68
ArianZdar <arian.zare-dar@polymtl.ca>
2025-02-01 15:49:35 -0500

    front working

---
+++ my-react-app/src/pages/aboutpage/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Stock Market Analyzer</h1>
+      <p>This app helps you analyze stock trends and make informed decisions.</p>
+    </div>
+  );
+}
+
+export default About;
