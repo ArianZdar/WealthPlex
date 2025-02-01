2aded5babacfdb8105aaad308d262a04a893d140
Tatok-n <tnhunter2004@gmail.com>
2025-02-01 12:01:12 -0500

    Refactored gradle and set basic endpoint

---
+++ backend/src/main/java/com/wealthPlex/WealthPlex/WebConfig.java
@@ -0,0 +1,16 @@
+package main.java.com.wealthPlex.WealthPlex;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Allow requests from your Flutter app
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow specific HTTP methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true); // Allow credentials (if needed)
+    }
+}
--- backend/src/main/java/com/wealthPlex/WealthPlex/core/controllers/UserController.java
+++ backend/src/main/java/com/wealthPlex/WealthPlex/core/controllers/UserController.java
@@ -1,6 +1,18 @@
 package main.java.com.wealthPlex.WealthPlex.core.controllers;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class UserController {
+
+
+
+
+    @GetMapping("/users/test")
+    public String testPath() {
+        return "hiiii";
+    }
+
 }
