667f91552c653c373bbcfa3061660998067754f1
ArianZdar <arian.zare-dar@polymtl.ca>
2025-02-01 16:25:10 -0500

    routers work

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Greeting from './components/greeting';
 import Home from './pages/homepage/home';
+import About from './pages/aboutpage/about'
 
 function App() {
   return (
+    <Router>
       <div className="App">
-        <Home></Home>
-        <h1>app page</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>
+    </Router>
   );
 }
 
