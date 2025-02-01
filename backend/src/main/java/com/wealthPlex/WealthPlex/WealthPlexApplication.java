ffe00b2e2d89539e2f42c38563d97764ff9a9500
Tatok-n <tnhunter2004@gmail.com>
2025-02-01 11:36:21 -0500

    Generated springboot project

---
+++ backend/src/main/java/com/wealthPlex/WealthPlex/WealthPlexApplication.java
@@ -0,0 +1,13 @@
+package com.wealthPlex.WealthPlex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthPlexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WealthPlexApplication.class, args);
+	}
+
+}
