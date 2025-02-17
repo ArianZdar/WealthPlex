fbec63dba327f2a9374a5120182f4e53f2f05dc6
Tatok-n <tnhunter2004@gmail.com>
2025-02-17 18:49:07 -0500

    Forgot to stage some stuff

---
+++ my-app/src/assets/utils/stockInfoRequests.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+
+const root = "http://localhost:8080";
+
+
+async function getStockRequests(symbol) {
+    try {
+        const response = await fetch(root + "/stocks/"+ symbol, {
+            method: "GET"
+        });
+
+        if (!response.ok) throw new Error("Failed to get stock recomendations");
+
+        const result = await response.json();
+        console.log("stock recomendations Response:", result);
+        return result;
+    } catch (error) {
+        console.error("stock recomendations error:", error.message);
+    }
+}
+
+
+
+    async function getStockRating(symbol) {
+        try {
+            const username = localStorage.getItem("loggedInUsername");
+            const response = await fetch(`${root}/api/stocks/rating/${symbol}?username=${username}`, {
+                method: "GET"
+            });
+    
+            if (!response.ok) throw new Error("Failed to get stock rating");
+            const result = await response.text();
+            console.log("stock rating Response:", result);
+            const extractedInt = parseInt(result.split("/")[0], 10);
+            return extractedInt;
+        } catch (error) {
+            console.error("stock rating error:", error.message);
+        }
+    }
+
+    async function getStockExplanation(symbol) {
+        try {
+            const username = localStorage.getItem("loggedInUsername");
+            const response = await fetch(`${root}/api/stocks/explanation/${symbol}?username=${username}`, {
+                method: "GET"
+            });
+    
+            if (!response.ok) throw new Error("Failed to get stock rating");
+            const result = await response.text();
+            return result;
+        } catch (error) {
+            console.error("stock rating error:", error.message);
+        }
+    }
+
+
+
+
+export {getStockRequests, getStockRating, getStockExplanation};
